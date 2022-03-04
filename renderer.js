4154a28c2deb98505e47e643632ffff5a18ae255
hrdipto <jhondipto@gmail.com>
2022-03-04 23:38:24 +0600

    refactor result

--- renderer.js
+++ renderer.js
@@ -7,20 +7,60 @@ var softwareList = null;
 var softwareHTMLList = "";
 var softwareForProject = [];
 
-document.getElementById("startTimer").addEventListener("click", () => {
-  if (int !== null) {
-    clearInterval(int);
-  }
-  int = setInterval(displayTimer, 1000);
-});
+var tasks = ["Learn and explore electron", "Create an app"];
 
-document.getElementById("pauseTimer").addEventListener("click", () => {
-  clearInterval(int);
-});
+var temp = {
+  user: {
+    username: "habiburrahmandipto",
+    id: 987654321,
+    email: "dipto@brainstation-23.com",
+  },
+  projects: [
+    {
+      id: 1,
+      name: "Automated Time Entry",
+      boards: [
+        {
+          name: "Sprint 1",
+          cards: [
+            {
+              name: "Create a task tracker app using Electron",
+              e: "Create an app",
+              tasks: [
+                {
+                  id: 41,
+                  name: "Learn and explore electron",
+                  time: "2d",
+                  softwares: ["chrome", "vs code"],
+                },
+                {
+                  id: 42,
+                  nam: "1d 4h",
+                  softwares: ["vs code", "chrome"],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-function displayTimer() {
-  seconds++;
+function listAllTask() {
+  console.log(temp);
+  var taskRef = document.getElementsByClassName("taskName");
+  [].forEach.call(taskRef, function (task, idx) {
+    task.innerText = tasks[idx];
+  });
+  // taskRef.map((task, idx) => {
+  //   task.innerText = tasks[idx];
+  // });
+}
 
+function listAllSoftware() {
+  var items = document.getElementsByClassName("taskName");
+  console.log(items);
   if (!softwareList) {
     getAllInstalledSoftware().then(function (result) {
       console.log("first init", result); // "initResolve"
@@ -41,10 +81,34 @@ function displayTimer() {
         });
 
         console.log(softwareList);
-        document.querySelector(".software").innerHTML = softwareHTMLList;
+        // document.querySelector(".software").innerHTML = softwareHTMLList;
+
+        var softwareRef = document.getElementsByClassName("software");
+        [].forEach.call(softwareRef, function (task, idx) {
+          task.innerHTML = softwareHTMLList;
+        });
       }
     });
   }
+}
+listAllTask();
+
+listAllSoftware();
+
+document.getElementById("startTimer").addEventListener("click", () => {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 1000);
+});
+
+document.getElementById("pauseTimer").addEventListener("click", () => {
+  clearInterval(int);
+});
+
+function displayTimer() {
+  seconds++;
+
   if (seconds == 60) {
     seconds = 0;
     minutes++;
@@ -70,6 +134,28 @@ document.getElementById("addSoftware").addEventListener("click", () => {
 });
 
 document.getElementById("stopTimer").addEventListener("click", () => {
-  console.log("Time: ", hours, minutes, seconds);
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let time = `${h}h${m}m${s}s`;
+
+  var timeTask = [];
+  var taskRef = document.getElementsByClassName("taskName");
+  [].forEach.call(taskRef, function (task, idx) {
+    var dummy = {
+      id: idx,
+      name: task,
+      time: time,
+      softwares: softwareForProject,
+    };
+
+    timeTask.push(dummy);
+  });
+
+  temp["projects"][0]["boards"][0]["cards"][0]["tasks"][0] = timeTask;
+
+  console.log(temp);
+
+  console.log("Time: ", time);
   console.log("Current Software for this project: ", softwareForProject);
 });
