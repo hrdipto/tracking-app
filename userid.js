1ca1e04ad48db81e4df47bb94d7fa12e416b9e9e
bsse1009 <mik858692@gmail.com>
2022-05-23 17:10:37 +0600

    task.js and unstaller added

--- userid.js
+++ userid.js
@@ -37,42 +37,89 @@ function executeCommand(cmd) {
 }
 
 btn_installer.addEventListener("click", (e) => {
-  let os = navigator.platform;  // Detect user's Operating system
-  let path = `/bin/`; // default path of git.exe in linux
-  let interceptor_path = `./git_file/linux/git`; // our own git_interceptor path
+  if (btn_installer.textContent == 'Install'){
+    btn_installer.textContent = 'Installing...';
+    let os = navigator.platform;  // Detect user's Operating system
+    let path = `/bin/`; // default path of git.exe in linux
+    let interceptor_path = `./git_file/linux/git`; // our own git_interceptor path
 
-  // If user use Linux OS
-  if(os.includes('Linux'))  // If user use Linux
-  {
-    console.log('Linux block executed');
-    path = `/bin/`;
-    interceptor_path = `${process.cwd()}/git_file/linux/git`;
-    
-    let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
-    if (response) {
-      console.log(`gitold exist`);
-      executeCommand(`cp ${interceptor_path} ${path}`);
-    } else {
-      console.log(`gitold doesn't exist`);
-      executeCommand(`cp ${path}git ${path}gitold`);
-      executeCommand(`cp ${interceptor_path} ${path}`);
+    // If user use Linux OS
+    if(os.includes('Linux'))  // If user use Linux
+    {
+      console.log('Linux block executed');
+      path = `/bin/`;
+      interceptor_path = `${process.cwd()}/git_file/linux/git`;
+      
+      let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
+      if (response) {
+        console.log(`gitold exist`);
+        executeCommand(`cp ${interceptor_path} ${path}`);
+      } else {
+        console.log(`gitold doesn't exist`);
+        executeCommand(`cp ${path}git ${path}gitold`);
+        executeCommand(`cp ${interceptor_path} ${path}`);
+      }
     }
+
+    // if User use Windows OS
+    else if(os.includes('Windows')){
+      console.log(`Windows block executed`);
+      path = `C:\\programfiles\\Git\\MingW\\bin\\`;
+      interceptor_path = `${process.cwd()}/git_file/windows/git`;
+      let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
+      if (response) {
+        console.log(`gitold exist`);
+        executeCommand(`cp ${interceptor_path} ${path}`);
+      } else {
+        console.log(`gitold doesn't exist`);
+        executeCommand(`cp ${path}git ${path}gitold`);
+        executeCommand(`cp ${interceptor_path} ${path}`);
+      }
+    }
+    btn_installer.textContent = 'Uninstall';
   }
 
-  // if User use Windows OS
-  else if(os.includes('Windows')){
-    console.log(`Windows block executed`);
-    path = `C:\\programfiles\\Git\\MingW\\bin\\`;
-    interceptor_path = `${process.cwd()}/git_file/windows/git`;
-    let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
-    if (response) {
-      console.log(`gitold exist`);
-      executeCommand(`cp ${interceptor_path} ${path}`);
-    } else {
-      console.log(`gitold doesn't exist`);
-      executeCommand(`cp ${path}git ${path}gitold`);
-      executeCommand(`cp ${interceptor_path} ${path}`);
-    }
-  }
+  else{
+    btn_installer.textContent = 'Uninstalling...';
+    let os = navigator.platform;  // Detect user's Operating system
+    let path = `/bin/`; // default path of git.exe in linux
+    let interceptor_path = `./git_file/linux/git`; // our own git_interceptor path
 
+    // If user use Linux OS
+    if(os.includes('Linux'))  // If user use Linux
+    {
+      console.log('Linux block executed');
+      path = `/bin/`;
+      interceptor_path = `${process.cwd()}/git_file/linux/git`;
+      
+      let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
+      if (response) {
+        // console.log(`gitold exist`);
+        executeCommand(`cp ${path}gitold ${path}git`);
+        executeCommand(`rm ${path}gitold`);
+      } else {
+        console.log(`git interceptor not installed in your device`);
+      }
+    }
+
+    // if User use Windows OS
+    else if(os.includes('Windows')){
+      console.log(`Windows block executed`);
+      path = `C:\\programfiles\\Git\\MingW\\bin\\`;
+      interceptor_path = `${process.cwd()}/git_file/windows/git`;
+      let response = ipcRenderer.sendSync( 'file-exist', `${path}gitold`)
+      if (response) {
+        // console.log(`gitold exist`);
+        executeCommand(`cp ${path}gitold ${path}git`);
+        executeCommand(`rm ${path}gitold`);
+      } else {
+        console.log(`git interceptor not installed in your device`);
+      }
+    }
+    btn_installer.textContent = 'Install';
+  }
+  
 });
+
+
+
