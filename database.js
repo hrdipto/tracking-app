aa6bfa57d7f35c8b881a134e5cd0fdb0e3f90f55
bsse1009 <mik858692@gmail.com>
2022-05-27 16:36:06 +0600

    update

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-<<<<<<< HEAD
   .connect("mongodb://127.0.0.1:27017/my_database", 
   { 
     useNewUrlParser: true,
@@ -11,9 +10,3 @@ mongoose
   .catch((err) => console.log(err));
 
 
-=======
-  .connect("mongodb://localhost:27017/test", { useNewUrlParser: true })
-  .then((db) => console.log("DB is connected"))
-  .catch((err) => console.log(err));
-
->>>>>>> 4e7bcfd86d23fff7d51b1fec62197adb60dab991
